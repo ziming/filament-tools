927891ede1db31ce263838fc93ebc9d56880278f
Ryan Chandler <ryangjchandler@gmail.com>
2022-01-21 18:52:45 +0000

    init

---
+++ src/FilamentToolsServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RyanChandler\FilamentTools;
+
+use Filament\PluginServiceProvider;
+
+class FilamentToolsServiceProvider extends PluginServiceProvider
+{
+    public static string $name = 'filament-tools';
+}
