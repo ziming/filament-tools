f216333e591d84b1b565bcf78e4f2e7d4346f18e
Ryan Chandler <ryangjchandler@gmail.com>
2022-01-23 00:51:58 +0000

    fix: render view inside of form

--- resources/views/components/tool.blade.php
+++ resources/views/components/tool.blade.php
@@ -1,15 +1,15 @@
 @props(['tool'])
 
 <x-filament::card :heading="$tool->getLabel()" class="col-span-{{ $tool->getColumnSpan() }}">
-    @if($tool->hasView())
-        {{ $tool->getView() }}
-    @else
-        <x-filament::form wire:submit.prevent="callToolSubmitAction({{ json_encode($tool->getId()) }})">
+    <x-filament::form wire:submit.prevent="callToolSubmitAction({{ json_encode($tool->getId()) }})">
+        @if($tool->hasView())
+            {{ $tool->getView() }}
+        @else
             {{ $this->getCachedForm($tool->getId()) }}
 
             <x-filament::button type="submit">
                 {{ $tool->getSubmitButtonLabel() }}
             </x-filament::button>
-        </x-filament::form>
-    @endif
+        @endif
+    </x-filament::form>
 </x-filament::card>
